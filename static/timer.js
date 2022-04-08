7b03783fdf9ef32d3c0dab4615a209927a215459
Asdrei <daniele.ieva@hotmail.com>
2022-04-08 22:53:13 +0200

    added color to progress bar

--- static/timer.js
+++ static/timer.js
@@ -1,31 +1,37 @@
 var time = 60;
+var time_tot = time;
 var running = false;
-document.getElementById("progressbar").innerHTML = time + 's';
+document.getElementById("time").innerHTML = time + 's';
 console.log(time);
 
 function add() {
     time += 15;
-    document.getElementById("progressbar").innerHTML = time + 's';
+    time_tot += 15;
+    document.getElementById("time").innerHTML = time + 's';
 }
 
 function rem() {
     time -= 15;
-    document.getElementById("progressbar").innerHTML = time + 's';
+    time_tot -= 15;
+    document.getElementById("time").innerHTML = time + 's';
 }
 
 function timer() {
     if (!running) {
         running = true;
         var timer = setInterval(function() {
-            time -= 1;
+            time -= 0.1;
+            
+            document.getElementById("greenbar").style.width = (1 - (time / time_tot)) * 100 + "%";
+            
             if (time <= 0) {
-                document.getElementById("progressbar").innerHTML = "Time is Over!";
+                document.getElementById("time").innerHTML = "Time is Over!";
                 clearInterval(timer);
             }
             else {
-                document.getElementById("progressbar").innerHTML = time + 's';
+                document.getElementById("time").innerHTML = Math.ceil(time) + 's';
                 running = false;
             }
-        }, 1000);
+        }, 100);
     }
 }
